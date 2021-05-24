9e54314241cd3100d27f66211af74f10fc1c12b5
Hemant <hemant.yadav@flynote.in>
2021-05-24 13:22:02 +0530

    chore(.env): add environment variable

--- setup.sh
+++ setup.sh
@@ -2,4 +2,7 @@ mkdir -p ../data/{es,pgadmin,pgdata}
 mkdir -p ../logs/es
 touch ../data/servers.json
 #Mac doesn't have sopport for relative symbolic link
-ln -sv `pwd`/docker-compose.yml `pwd`/../docker-compose.yml
+# ln -sv `pwd`/docker-compose.yml `pwd`/../template.yml
+set -a
+source .env
+envsubst < docker-compose.yml > ../docker-compose.yml
